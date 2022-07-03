c91e862fbd67e5d73d6702d95ddfff057a5aee3a
Mr.beam <70924959+mrbeamuse@users.noreply.github.com>
2022-07-03 17:14:45 +0800

    添加prettier，使用prettier校验规则，添加husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,14 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:vue/vue3-recommended",
-    "plugin:@typescript-eslint/recommended",
-  ],
+  extends: ['plugin:vue/vue3-recommended', 'plugin:prettier/recommended'],
   parserOptions: {
-    ecmaVersion: "latest",
-    parser: "@typescript-eslint/parser",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
   },
-  plugins: ["vue", "@typescript-eslint"],
-  rules: {},
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {}
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite"
-import vue from "@vitejs/plugin-vue"
-import vueJsx from "@vitejs/plugin-vue-jsx"
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +8,6 @@ export default defineConfig({
     vue(),
     vueJsx({
       // options are passed on to @vue/babel-plugin-jsx
-    }),
-  ],
+    })
+  ]
 })
